b96c0fde3fcc66f566a064c5e94b4acb694ff9b1
Ephrem0001 <ephrem8118@gmail.com>
2025-07-30 22:17:49 +0300

    Your commit message

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,5 +1,8 @@
-
 AUTH_USER_MODEL = 'chats.User'
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -9,3 +12,16 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
