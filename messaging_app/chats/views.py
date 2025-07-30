0019bc776817053641e954fb569e0fde95aca6f2
Ephrem0001 <ephrem8118@gmail.com>
2025-07-30 15:31:58 +0300

    Completed

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """
+        Automatically add the requesting user as one of the participants.
+        """
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Optionally filter messages by conversation ID.
+        Example: /api/messages/?conversation=<conversation_id>
+        """
+        queryset = Message.objects.all()
+        conversation_id = self.request.query_params.get('conversation')
+        if conversation_id:
+            queryset = queryset.filter(conversation__conversation_id=conversation_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        """
+        Attach the authenticated user as the sender.
+        """
+        serializer.save(sender=self.request.user)
